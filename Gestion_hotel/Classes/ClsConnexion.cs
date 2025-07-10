d84954fd9fc8435bd1109587aebc541a5787bedd
Thibaut Tbc Bujiriri <166028783+thibaut-tbc-bujiriri@users.noreply.github.com>
2025-07-10 17:05:50 +0200

    update

--- Gestion_hotel/Classes/ClsConnexion.cs
+++ Gestion_hotel/Classes/ClsConnexion.cs
@@ -9,5 +9,9 @@ namespace Gestion_hotel.Classes
     internal class ClsConnexion
     {
         public static string Way = "Server = DESKTOP1972; Database=Gestion_hotel;User Id = sa; Password=1234; encrypt=false";
+        public int tib(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
